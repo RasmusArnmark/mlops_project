f29a20f231f67882559d205bfccb336244d8cc30
lyngsberg <Eliasrasmussen12345@gmail.com>
2025-01-24 17:10:37 +0100

    report

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -3,9 +3,10 @@ from locust import HttpUser, task, between
 class FastAPILoadTestUser(HttpUser):
     wait_time = between(1, 2)  # Simulate a delay between requests
     host = "http://127.0.0.1:8000"  # Set the host to your FastAPI app's URL
+    # host = "https://food-classification-api-654702382857.europe-west1.run.app/"
 
     @task
     def test_predict(self):
         """Send a test image to the /predict/ endpoint."""
-        with open("test_image/sample.jpg", "rb") as img:  # Replace with your test image path
+        with open("tests/test_image/sample.jpg", "rb") as img:  # Replace with your test image path
             self.client.post("/predict/", files={"file": img})
