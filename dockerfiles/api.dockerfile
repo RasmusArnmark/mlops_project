86d7c0d2b2276adec0397d3ab8d6c24a27e48947
Rasmus Arnmark <rasmusarnmark@gmail.com>
2025-01-24 10:02:03 +0100

    prometheus func

--- dockerfiles/api.dockerfile
+++ dockerfiles/api.dockerfile
@@ -1,24 +1,20 @@
-# Base image with Python 3.11
 FROM python:3.11-slim AS base
 
-# Install necessary system dependencies for building Python packages
 RUN apt update && \
     apt install --no-install-recommends -y build-essential gcc && \
     apt clean && rm -rf /var/lib/apt/lists/*
 
-# Set the working directory inside the container
 WORKDIR /app
 
-# Copy only the requirements file first to cache pip installations
 COPY requirements.txt /app/
 
-# Install Python dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of the application code
 COPY . /app
 
-# Expose the port Gradio will run on (8080 for Cloud Run)
 EXPOSE 8080
+EXPOSE 8001  
+
+# expose 8001 for prometheus metrics
 
 CMD ["python", "-m", "src.gradio"]
--- src/gradio.py
+++ src/gradio.py
@@ -3,16 +3,29 @@ from PIL import Image
 import torch
 from src.model_handler import load_model, predict_image, CLASS_MAPPING
 from src.utils import preprocess_image
+from prometheus_client import Counter, Histogram, start_http_server
+import time
+
+
+REQUEST_COUNT = Counter("request_count", "Number of prediction requests")
+REQUEST_LATENCY = Histogram("request_latency_seconds", "Latency of prediction requests")
+ERROR_COUNT = Counter("error_count", "Number of failed prediction requests")
+
+start_http_server(8001)  # Port for metrics (accessible at /metrics)
 
 # Load the model
 model_path = "models/food_cnn.pth"  # Adjust the path as needed
 model = load_model(model_path, bucket_name="foodclassrae")
 
 # Define the function Gradio will use
+
 def gradio_interface(image):
     """
     Function for Gradio to handle an image input and return a prediction.
     """
+    REQUEST_COUNT.inc()  # Increment request count
+    start_time = time.time()  # Start timer to measure latency
+    
     try:
         # Directly preprocess the PIL image
         processed_image = preprocess_image(image)
@@ -20,8 +33,12 @@ def gradio_interface(image):
         # Predict using the loaded model
         prediction = predict_image(processed_image, model, CLASS_MAPPING)
 
+        latency = time.time() - start_time  # Calculate request latency
+        REQUEST_LATENCY.observe(latency)  # Log latency to Prometheus
+
         return f"Predicted Class: {prediction}"
     except Exception as e:
+        ERROR_COUNT.inc()  # Increment error count
         return f"Error during prediction: {str(e)}"
 
 # Set up Gradio interface
